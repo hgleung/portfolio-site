49f19d72da29b297bad9ffd86b0dc1691ae27582
Harry Leung <hleung.cs@gmail.com>
2024-07-13 16:56:29 -0700

    database set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 
 export default function HomePage() {
+  // const posts = await db.query.posts.findMany();
+
+  // console.log(posts);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-ivory text-black">
       Hello (in progress)
