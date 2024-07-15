c830186834b9f228714274c3be8a7612e836b9d4
Harry Leung <hleung.cs@gmail.com>
2024-07-14 17:47:21 -0700

    Links moved to middle of page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,46 @@ export default function HomePage() {
 
   // console.log(posts);
   return (
-    <main className="flex max-w-lg flex-col items-center self-center justify-center bg-ivory text-black">
+    <main className="flex flex-col items-center self-center justify-center bg-ivory text-black">
+      <div className='max-w-lg pb-10'>
       Hey, I'm Harry! I am currently studying Computer Science at UC Irvine with a specialization in Intelligent Systems.
       In the past, I've worked in Software QA as an intern at Litepoint. I am currently located in the Bay Area. Feel free to reach out!
-      <Footer />
+      </div>
+      <div>
+      </div>
+      <div className="grid items-start grid-cols-1 md:grid-cols-12 gap-3 md:gap-12">
+        <div className="col-span-12">
+          <div className="flex flex-col space-y-3">
+            <a href="mailto:hleung.cs@gmail.com" className="flex items-center space-x-4 group">
+              <strong className="flex-none font-medium" >
+                Email: hleung.cs@gmail.com
+              </strong>
+            <span className="w-full border-t border-gray-300 border-dashed shrink dark:border-gray-800"></span>
+            <span className="flex-none text-tertiary text-sm md:text-base">
+              Send
+            </span>
+            </a>
+            <a href="https://github.com/hgleung" className="flex items-center space-x-4 group">
+              <strong className="flex-none font-medium">
+                GitHub
+              </strong>
+            <span className="w-full border-t border-gray-300 border-dashed shrink dark:border-gray-800"></span>
+            <span className="flex-none text-tertiary text-sm md:text-base">
+              Follow
+            </span>
+            </a>
+            <a href="https://www.linkedin.com/in/harrygleung/" className="flex items-center space-x-4 group">
+              <strong className="flex-none font-medium">
+                LinkedIn
+              </strong>
+            <span className="w-full border-t border-gray-300 border-dashed shrink dark:border-gray-800"></span>
+            <span className="flex-none text-tertiary text-sm md:text-base">
+              Connect
+            </span>
+            </a>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
