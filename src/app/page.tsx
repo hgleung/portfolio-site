e8dc6631c5ad3f40687fe271780b6e8ebf2440ba
Harry Leung <hleung.cs@gmail.com>
2024-07-16 20:50:45 -0700

    'Online' Heading added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,24 +15,23 @@ export default function HomePage() {
 
   // console.log(posts);
   return (
-    <main className="flex font-light flex-col items-center self-center justify-center bg-ivory text-charcoal">
-      <div>
-        {/* <img src="./head.png" width={200} height={200} className='pb-4'></img> */}
-      </div>
-      <div className='max-w-64 md:max-w-lg md:flex pb-10'>
-      Hey, I'm Harry! I am currently studying Computer Science at UC Irvine with a specialization in Intelligent Systems.
-      In the past, I've worked in Software QA as an intern at Litepoint. I am currently located in the Bay Area. Feel free to reach out!
-      </div>
-      <div>
-      </div>
-      <div className="grid items-start grid-cols-1 md:grid-cols-12 gap-3 md:gap-12">
-        <div className="col-span-12">
+    <main className="max-w-xl font-light items-center self-center justify-center bg-ivory text-charcoal">
+      <div className='grid items-start grid-cols-1 md:grid-cols-12 pb-4 md:pb-16'>
+        <h4 className='col-span-2'> </h4>
+        <div className='col-span-10'>
+        Hey, I'm Harry! I am currently studying Computer Science at UC Irvine with a specialization in Intelligent Systems.
+        In the past, I've worked in Software QA as an intern at Litepoint. I am currently located in the Bay Area. Feel free to reach out!
+        </div>
+        </div>
+      <div className="grid items-start grid-cols-1 md:grid-cols-12 pb-4 md:pb-16">
+        <h4 className='col-span-2'>Online</h4>
+        <div className="col-span-10">
           <div className="flex flex-col space-y-3">
             <a href="mailto:hleung.cs@gmail.com" className="flex items-center space-x-4 group">
-              <strong className="flex-none font-normal" >
+              <strong className="flex-none font-normal">
                 Email: hleung.cs@gmail.com
               </strong>
-            <span className="w-full border-t border-gray-300 border-dashed shrink dark:border-gray-800"></span>
+            <span className="w-full border-t border-gray-800 border-dashed shrink"></span>
             <span className="flex-none text-tertiary text-sm md:text-base">
               Send
             </span>
@@ -41,7 +40,7 @@ export default function HomePage() {
               <strong className="flex-none font-normal">
                 GitHub
               </strong>
-            <span className="w-full border-t border-gray-300 border-dashed shrink dark:border-gray-800"></span>
+            <span className="w-full border-t border-gray-800 border-dashed shrink"></span>
             <span className="flex-none text-tertiary text-sm md:text-base">
               Follow
             </span>
@@ -50,12 +49,18 @@ export default function HomePage() {
               <strong className="flex-none font-normal">
                 LinkedIn
               </strong>
-            <span className="w-full border-t border-gray-300 border-dashed shrink dark:border-gray-800"></span>
+            <span className="w-full border-t border-gray-800 border-dashed shrink"></span>
             <span className="flex-none text-tertiary text-sm md:text-base">
               Connect
             </span>
             </a>
           </div>
+        </div>
+      </div>
+      <div className="grid items-start grid-cols-1 md:grid-cols-12">
+        <h4 className='col-span-2'>Where</h4>
+        <div className="col-span-10">
+    
         </div>
       </div>
     </main>
