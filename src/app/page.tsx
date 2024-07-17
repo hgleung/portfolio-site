eb35a94f6ed3f7c0735e42ef2bab4b031389e6ce
Harry Leung <hleung.cs@gmail.com>
2024-07-16 21:06:21 -0700

    mobile formatting fix #3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,39 +16,39 @@ export default function HomePage() {
   // console.log(posts);
   return (
     <main className="max-w-80 md:max-w-xl font-light items-center self-center justify-center bg-ivory text-charcoal">
-      <div className='grid items-start grid-cols-1 md:grid-cols-12 pb-4 md:pb-16'>
+      <div className='grid items-start grid-cols-1 gap-6 md:grid-cols-12 pb-4 md:pb-16'>
         <h4 className='col-span-2'> </h4>
         <div className='col-span-10'>
         Hey, I'm Harry! I am currently studying Computer Science at UC Irvine with a specialization in Intelligent Systems.
         In the past, I've worked in Software QA as an intern at Litepoint. I am currently located in the Bay Area. Feel free to reach out!
         </div>
         </div>
-      <div className="grid items-start grid-cols-1 md:grid-cols-12 pb-4 md:pb-16">
-        <h4 className='col-span-2'>Online</h4>
+      <div className="grid items-start grid-cols-1 gap-6 md:grid-cols-12 pb-8 md:pb-16">
+        <h4 className='col-span-2 font-extrabold md:font-medium pt-8 md:pt-0'>Online</h4>
         <div className="col-span-10">
           <div className="flex flex-col space-y-3">
             <a href="mailto:hleung.cs@gmail.com" className="flex items-center space-x-2 md:space-x-4 group">
-              <strong className="flex-none font-normal">
+              <a className="flex-none font-normal md:font-light">
                 Email: hleung.cs@gmail.com
-              </strong>
+              </a>
             <span className="w-full border-t border-gray-800 border-dashed shrink"></span>
             <span className="flex-none text-tertiary text-sm md:text-base">
               Send
             </span>
             </a>
             <a href="https://github.com/hgleung" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-4 group">
-              <strong className="flex-none font-normal">
+              <a className="flex-none font-normal md:font-light">
                 GitHub
-              </strong>
+              </a>
             <span className="w-full border-t border-gray-800 border-dashed shrink"></span>
             <span className="flex-none text-tertiary text-sm md:text-base">
               Follow
             </span>
             </a>
             <a href="https://www.linkedin.com/in/harrygleung/" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-4 group">
-              <strong className="flex-none font-normal">
+              <a className="flex-none font-normal md:font-light">
                 LinkedIn
-              </strong>
+              </a>
             <span className="w-full border-t border-gray-800 border-dashed shrink"></span>
             <span className="flex-none text-tertiary text-sm md:text-base">
               Connect
