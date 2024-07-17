8ef30e41782301c01db0867ff5a120161c3a52bc
Harry Leung <hleung.cs@gmail.com>
2024-07-16 20:53:17 -0700

    repushing online heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,12 +57,12 @@ export default function HomePage() {
           </div>
         </div>
       </div>
-      <div className="grid items-start grid-cols-1 md:grid-cols-12">
+      {/* <div className="grid items-start grid-cols-1 md:grid-cols-12">
         <h4 className='col-span-2'>Where</h4>
         <div className="col-span-10">
     
         </div>
-      </div>
+      </div> */}
     </main>
   );
 }
